2e1cf96c8dd936e668885282478c28d14e436bad
Julian Ceballos <cristianjulianceballos@gmail.com>
2016-04-19 04:15:37 -0500

    add error handling

--- conekta/Base/Requestor.cs
+++ conekta/Base/Requestor.cs
@@ -16,28 +16,48 @@ namespace conekta
 
 		public String request (String method, String resource_uri, String data = "{}")
 		{
-			HttpWebRequest http = (HttpWebRequest)WebRequest.Create(conekta.Api.baseUri + resource_uri);
-			http.Accept = "application/vnd.conekta-v" + conekta.Api.version + "+json";
-			http.UserAgent = "Conekta/v1 DotNetBindings/Conekta::" + conekta.Api.version;
-			http.Method = method;
+			try {
+				HttpWebRequest http = (HttpWebRequest)WebRequest.Create(conekta.Api.baseUri + resource_uri);
+				http.Accept = "application/vnd.conekta-v" + conekta.Api.version + "+json";
+				http.UserAgent = "Conekta/v1 DotNetBindings/Conekta::" + conekta.Api.version;
+				http.Method = method;
 
-			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(conekta.Api.apiKey);
-			http.Headers.Add("Authorization", "Basic " + System.Convert.ToBase64String(plainTextBytes) + ":");
-			http.Headers.Add ("Accept-Language", conekta.Api.locale);
+				var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(conekta.Api.apiKey);
+				http.Headers.Add("Authorization", "Basic " + System.Convert.ToBase64String(plainTextBytes) + ":");
+				http.Headers.Add ("Accept-Language", conekta.Api.locale);
 
-			if (method == "POST" || method == "PUT") {
-				var dataBytes = Encoding.ASCII.GetBytes(data);
+				if (method == "POST" || method == "PUT") {
+					var dataBytes = Encoding.ASCII.GetBytes(data);
 
-				http.ContentLength = dataBytes.Length;
-				http.ContentType = "application/json";
+					http.ContentLength = dataBytes.Length;
+					http.ContentType = "application/json";
 
-				Stream dataStream = http.GetRequestStream ();
-				dataStream.Write (dataBytes, 0, dataBytes.Length);
+					Stream dataStream = http.GetRequestStream ();
+					dataStream.Write (dataBytes, 0, dataBytes.Length);
+				}
+
+				WebResponse response = http.GetResponse ();
+				var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+				return responseString;	
+			} catch (WebException webExcp) {
+				WebExceptionStatus status =  webExcp.Status;
+
+				if (status == WebExceptionStatus.ProtocolError) {
+					HttpWebResponse httpResponse = (HttpWebResponse)webExcp.Response;
+
+					var encoding = ASCIIEncoding.ASCII;
+					using (var reader = new System.IO.StreamReader(httpResponse.GetResponseStream(), encoding))
+					{
+						string responseText = reader.ReadToEnd();
+
+						throw new Exception ("Conekta API Implementation Error (" + (int)httpResponse.StatusCode + " - " + httpResponse.StatusCode + ") :: " + responseText);
+					}
+				}
+				return "";
+			} catch	(Exception e) {
+				System.Console.WriteLine (e.ToString ());
+				return "";
 			}
-
-			WebResponse response = http.GetResponse ();
-			var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-			return responseString;
 		}
 
 	}
