839f20dae47b93ea88f52f46b80f4546af8fa501
Gustavilo <gustavo.alfaro@conekta.com>
2021-03-11 15:16:36 -0600

    Checkout implementation.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1](https://github.com/conekta/conekta-.net/releases/tag/1.2.0) - 2021-03-11
+### Feature
+- Checkout implementation.
+
 ## [1.0.1](https://github.com/conekta/conekta-.net/releases/tag/1.0.1) - 2017-09-05
 ### Changed
 - Re enabling charges for API v1.
--- src/conekta/conekta/Models/Order.cs
+++ src/conekta/conekta/Models/Order.cs
@@ -1,138 +1,142 @@
 ﻿using System.Text.RegularExpressions;
-
+using conekta.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
 
 namespace conekta
 {
-	public class Order : Resource
-	{
-		public string id { get; set; }
-		public int amount { get; set; }
-		public string currency { get; set; }
-		public string payment_status { get; set; }
-		public ConektaList line_items { get; set; }
-		public ConektaList shipping_lines { get; set; }
-		public ConektaList tax_lines { get; set; }
-		public ConektaList discount_lines { get; set; }
-		public string customer_id { get; set; }
-		public Customer customer_info { get; set; }
-		public string shipping_contact_id { get; set; }
-		public ShippingContact shipping_contact { get; set; }
-		public FiscalEntity fiscal_entity { get; set; }
-		public ConektaList charges { get; set; }
-		public ConektaList returns { get; set; }
-		public string _object { get; set; }
-		public JObject metadata { get; set; }
+    public class Order : Resource
+    {
+        public string id { get; set; }
+        public int amount { get; set; }
+        public string currency { get; set; }
+        public string payment_status { get; set; }
+        public ConektaList line_items { get; set; }
+        public ConektaList shipping_lines { get; set; }
+        public ConektaList tax_lines { get; set; }
+        public ConektaList discount_lines { get; set; }
+        public string customer_id { get; set; }
+        public Customer customer_info { get; set; }
+        public string shipping_contact_id { get; set; }
+        public ShippingContact shipping_contact { get; set; }
+        public FiscalEntity fiscal_entity { get; set; }
+        public ConektaList charges { get; set; }
+        public ConektaList returns { get; set; }
+        public string _object { get; set; }
+        public JObject metadata { get; set; }
 
-		public Order()
-		{
-			this.line_items = new ConektaList(typeof(conekta.LineItem));
-			this.shipping_lines = new ConektaList(typeof(conekta.ShippingLine));
-			this.tax_lines = new ConektaList(typeof(conekta.TaxLine));
-			this.discount_lines = new ConektaList(typeof(conekta.DiscountLine));
-			this.charges = new ConektaList(typeof(conekta.Charge));
-			this.returns = new ConektaList(typeof(conekta.Return));
-		}
+        /// <summary>
+        /// Gets or sets the checkout.
+        /// </summary>
+        public Checkout checkout { get; set; }
 
-		public Order create(string data)
-		{
-			string charge = this.create("/orders", data);
-			return this.toClass(this.toObject(charge).ToString());
-		}
+        public Order()
+        {
+            this.line_items = new ConektaList(typeof(conekta.LineItem));
+            this.shipping_lines = new ConektaList(typeof(conekta.ShippingLine));
+            this.tax_lines = new ConektaList(typeof(conekta.TaxLine));
+            this.discount_lines = new ConektaList(typeof(conekta.DiscountLine));
+            this.charges = new ConektaList(typeof(conekta.Charge));
+            this.returns = new ConektaList(typeof(conekta.Return));
+        }
 
-		public Order find(string id)
-		{
-			string order = this.find("/orders", id);
-			return this.toClass(this.toObject(order).ToString());
-		}
+        public Order create(string data)
+        {
+            string charge = this.create("/orders", data);
+            return this.toClass(this.toObject(charge).ToString());
+        }
 
-		public Order[] where(JObject data)
-		{
-			string result = this.where("/orders", data.ToString());
+        public Order find(string id)
+        {
+            string order = this.find("/orders", id);
+            return this.toClass(this.toObject(order).ToString());
+        }
 
-			Regex pattern = new Regex("\"object\":", RegexOptions.Multiline | RegexOptions.IgnoreCase);
-			result = pattern.Replace(result, "\"_object\":");
+        public Order[] where(JObject data)
+        {
+            string result = this.where("/orders", data.ToString());
 
-			JObject ords = JObject.Parse(result);
+            Regex pattern = new Regex("\"object\":", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            result = pattern.Replace(result, "\"_object\":");
 
-			JArray list = (JArray)ords["data"];
+            JObject ords = JObject.Parse(result);
 
-			Order[] orders = new Order[list.Count];
+            JArray list = (JArray)ords["data"];
 
-			int counter = 0;
+            Order[] orders = new Order[list.Count];
 
-			foreach (var item in ords.GetValue("data"))
-			{
-				orders[counter] = this.toClass(this.toObject(item.ToString()).ToString());
-				counter++;
-			}
-			
-			return orders;
-		}
+            int counter = 0;
 
-		public Order update(string data)
-		{
-			Order order = this.toClass(this.toObject(this.update("/orders/" + this.id, data)).ToString());
-			this.id = order.id;
-			return this;
-		}
+            foreach (var item in ords.GetValue("data"))
+            {
+                orders[counter] = this.toClass(this.toObject(item.ToString()).ToString());
+                counter++;
+            }
 
-		public Order capture()
-		{
-			string charge = this.request("PUT", "/orders/" + this.id + "/capture", @"{}");
-			return this.toClass(this.toObject(charge).ToString());
-		}
+            return orders;
+        }
 
-		public Order createReturn(string data)
-		{
-			string charge = this.request("POST", "/orders/" + this.id + "/refunds", data);
-			return this.toClass(this.toObject(charge).ToString());
-		}
+        public Order update(string data)
+        {
+            Order order = this.toClass(this.toObject(this.update("/orders/" + this.id, data)).ToString());
+            this.id = order.id;
+            return this;
+        }
 
-		public Charge createCharge(string data)
-		{
-			string charge = this.request("POST", "/orders/" + this.id + "/charges", data);
-			Charge skeleton = new Charge();
-			return skeleton.toClass(skeleton.toObject(charge).ToString());
-		}
+        public Order capture()
+        {
+            string charge = this.request("PUT", "/orders/" + this.id + "/capture", @"{}");
+            return this.toClass(this.toObject(charge).ToString());
+        }
 
-		public LineItem createLineItem(string data)
-		{
-			string line_item = this.request("POST", "/orders/" + this.id + "/line_items", data);
-			LineItem skeleton = new LineItem();
-			return skeleton.toClass(skeleton.toObject(line_item).ToString());
-		}
+        public Order createReturn(string data)
+        {
+            string charge = this.request("POST", "/orders/" + this.id + "/refunds", data);
+            return this.toClass(this.toObject(charge).ToString());
+        }
 
-		public TaxLine createTaxLine(string data)
-		{
-			string tax_line = this.request("POST", "/orders/" + this.id + "/tax_lines", data);
-			TaxLine skeleton = new TaxLine();
-			return skeleton.toClass(skeleton.toObject(tax_line).ToString());
-		}
+        public Charge createCharge(string data)
+        {
+            string charge = this.request("POST", "/orders/" + this.id + "/charges", data);
+            Charge skeleton = new Charge();
+            return skeleton.toClass(skeleton.toObject(charge).ToString());
+        }
 
-		public ShippingLine createShippingLine(string data)
-		{
-			string shipping_line = this.request("POST", "/orders/" + this.id + "/shipping_lines", data);
-			ShippingLine skeleton = new ShippingLine();
-			return skeleton.toClass(skeleton.toObject(shipping_line).ToString());
-		}
+        public LineItem createLineItem(string data)
+        {
+            string line_item = this.request("POST", "/orders/" + this.id + "/line_items", data);
+            LineItem skeleton = new LineItem();
+            return skeleton.toClass(skeleton.toObject(line_item).ToString());
+        }
 
-		public DiscountLine createDiscountLine(string data)
-		{
-			string discount_line = this.request("POST", "/orders/" + this.id + "/discount_lines", data);
-			DiscountLine skeleton = new DiscountLine();
-			return skeleton.toClass(skeleton.toObject(discount_line).ToString());
-		}
+        public TaxLine createTaxLine(string data)
+        {
+            string tax_line = this.request("POST", "/orders/" + this.id + "/tax_lines", data);
+            TaxLine skeleton = new TaxLine();
+            return skeleton.toClass(skeleton.toObject(tax_line).ToString());
+        }
 
-		public Order toClass(string json)
-		{
-			Order order = JsonConvert.DeserializeObject<Order>(json, new JsonSerializerSettings
-			{
-				NullValueHandling = NullValueHandling.Ignore
-			});
-			return order;
-		}
-	}
+        public ShippingLine createShippingLine(string data)
+        {
+            string shipping_line = this.request("POST", "/orders/" + this.id + "/shipping_lines", data);
+            ShippingLine skeleton = new ShippingLine();
+            return skeleton.toClass(skeleton.toObject(shipping_line).ToString());
+        }
+
+        public DiscountLine createDiscountLine(string data)
+        {
+            string discount_line = this.request("POST", "/orders/" + this.id + "/discount_lines", data);
+            DiscountLine skeleton = new DiscountLine();
+            return skeleton.toClass(skeleton.toObject(discount_line).ToString());
+        }
+
+        public Order toClass(string json)
+        {
+            Order order = JsonConvert.DeserializeObject<Order>(json, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            return order;
+        }
+    }
 }
