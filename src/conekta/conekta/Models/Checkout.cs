73aa3a129181037b0fe5a9700be596cf57078def
Gustavilo <tavo@conekta.com>
2021-03-17 11:08:16 -0600

    Checkout implementation. (#53)

---
+++ src/conekta/conekta/Models/Checkout.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace conekta.Models
+{
+    /// <summary>
+    /// Checkout.
+    /// </summary>
+    public class Checkout : Resource
+    {
+        #region :: Properties ::
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public string id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the livemode.
+        /// </summary>
+        public bool livemode { get; set; }
+
+        /// <summary>
+        /// Gets or sets if the emails sents.
+        /// </summary>
+        public int emails_sent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the paid payments count.
+        /// </summary>
+        public int paid_payments_count { get; set; }
+
+        /// <summary>
+        /// Gets or sets the payments limit count.
+        /// </summary>
+        public int payments_limit_count { get; set; }
+
+        /// <summary>
+        /// Gets or sets if the sms sents.
+        /// </summary>
+        public int sms_sent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        public string status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        public string type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recurrent.
+        /// </summary>
+        public bool recurrent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expires at.
+        /// </summary>
+        public string expires_at { get; set; }
+
+        /// <summary>
+        /// Gets or sets the allowed payment methods.
+        /// </summary>
+        public string[] allowed_payment_methods { get; set; }
+
+        /// <summary>
+        /// Gets or sets if needs shipping contact.
+        /// </summary>
+        public bool needs_shipping_contact { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        public int[] monthly_installments_options { get; set; }
+
+        /// <summary>
+        /// Gets or sets if monthly installments are enabled.
+        /// </summary>
+        public bool monthly_installments_enabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets flag for on demand enabled.
+        /// </summary>
+        public bool on_demand_enabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expired at.
+        /// </summary>
+        public int expired_at { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slug.
+        /// </summary>
+        public string slug { get; set; }
+
+        /// <summary>
+        /// Gets or sets the url.
+        /// </summary>
+        public string url { get; set; }
+
+        /// <summary>
+        /// Gets or set the object.
+        /// </summary>
+        public string _object { get; set; }
+
+        #endregion
+
+        #region :: Methods ::
+
+        /// <summary>
+        /// Creates a checkout.
+        /// </summary>
+        /// <param name="data">Parameters.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout create(string data)
+        {
+            string checkout = create("/checkouts", data);
+            return toClass(checkout);
+        }
+
+        /// <summary>
+        /// Finds a checkout.
+        /// </summary>
+        /// <param name="id">Checkout id.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout find(string id)
+        {
+            string checkout = find($"/checkouts", id);
+            return toClass(checkout);
+        }
+
+        /// <summary>
+        /// Gets arrar of chekout.
+        /// </summary>
+        /// <param name="data">Parameters</param>
+        /// <returns>Arrar of Checkout.</returns>
+        public Checkout[] where(JObject data)
+        {
+            string result = where("/checkouts", data.ToString());
+
+            result = result.Replace("\"object\"", "\"_object\"");
+
+            JObject ords = JObject.Parse(result);
+
+            JArray list = (JArray)ords["data"];
+
+            List<Checkout> checkouts = new List<Checkout>();
+
+            foreach (var item in list)
+            {
+                checkouts.Add(toClass(JsonConvert.SerializeObject(item)));
+            }
+
+            return checkouts.ToArray();
+        }
+
+        /// <summary>
+        /// Sends sms.
+        /// </summary>
+        /// <param name="data">Parameters.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout send_sms(string data)
+        {
+            string checkout = request("POST", $"/checkouts/{id}/sms", data);
+            return toClass(checkout);
+        }
+
+        /// <summary>
+        /// Sends email.
+        /// </summary>
+        /// <param name="data">Parameters.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout send_email(string data)
+        {
+            string checkout = request("POST", $"/checkouts/{id}/email", data);
+            return toClass(checkout);
+        }
+
+        /// <summary>
+        /// Cancel checkout.
+        /// </summary>
+        /// <param name="data">Parameters.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout cancel(string data)
+        {
+            string checkout = request("PUT", $"/checkouts/{id}/cancel", data);
+            return toClass(checkout);
+        }
+
+        /// <summary>
+        /// Convert string to class.
+        /// </summary>
+        /// <param name="json">String to convert.</param>
+        /// <returns>Checkout object.</returns>
+        public Checkout toClass(string json)
+        {
+            json = json.Replace("\"object\"", "\"_object\"");
+
+            Checkout checkout = JsonConvert.DeserializeObject<Checkout>(json,
+              new JsonSerializerSettings
+              {
+                  NullValueHandling = NullValueHandling.Ignore
+              });
+
+            return checkout;
+        }
+
+        #endregion
+    }
+}
--- src/conekta/conekta/Test.cs
+++ src/conekta/conekta/Test.cs
@@ -1,11 +1,10 @@
-﻿    using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
-
-    using conekta;
+﻿using NUnit.Framework;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using conekta;
+using conekta.Models;
+using System.Linq;
 
 namespace ConektaTest
 {
@@ -146,9 +145,183 @@ namespace ConektaTest
     [TestFixture()]
     public class OrderTest
     {
-        public static void getApiKey()
+        #region :: Private Fields ::
+
+        /// <summary>
+        /// Api Key.
+        /// </summary>
+        private readonly string _apikey = "key_ZLy4aP2szht1HqzkCezDEA";
+
+        /// <summary>
+        /// Order data with checkout.
+        /// </summary>
+        private readonly string _orderDataWithCheckout;
+
+        /// <summary>
+        /// Order data with checkout and MSI.
+        /// </summary>
+        private readonly string _orderDataWithMSI;
+
+        /// <summary>
+        /// Order data with checkout and saved card.
+        /// </summary>
+        private readonly string _orderDataWithSavedCard;
+
+        #endregion
+
+        #region :: Constructor ::
+
+        /// <summary>
+        /// Creates an instance of OrderTest.
+        /// </summary>
+        public OrderTest()
         {
-            conekta.Api.apiKey = "key_ZLy4aP2szht1HqzkCezDEA";
+            var customerInfo = new
+            {
+                customer_id = "cus_2o8jK3TDtejmz1sYb"
+            };
+
+            var lineItems = new[]
+            {
+                new
+                {
+                    name = "Box of Cohiba S1s",
+                    unit_price = 35000,
+                    quantity = 1,
+                }
+            };
+
+            var checkoutData = new
+            {
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" }
+            };
+
+            var checkoutDataMSI = new
+            {
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                monthly_installments_enabled = true,
+                monthly_installments_options = new int[] { 3, 6, 12 },
+            };
+
+            var checkoutDataSaveCard = new
+            {
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                on_demand_enabled = true
+            };
+
+            var orderDataWithCheckout = new
+            {
+                currency = "MXN",
+                line_items = lineItems,
+                metadata = new
+                {
+                    test = true
+                },
+                checkout = checkoutData,
+                customer_info = customerInfo
+            };
+
+            var orderDataWithMSI = new
+            {
+                currency = "MXN",
+                line_items = lineItems,
+                metadata = new
+                {
+                    test = true
+                },
+                checkout = checkoutDataMSI,
+                customer_info = customerInfo
+            };
+
+            var orderDataWithSavedCard = new
+            {
+                currency = "MXN",
+                line_items = lineItems,
+                metadata = new
+                {
+                    test = true
+                },
+                checkout = checkoutDataSaveCard,
+                customer_info = customerInfo
+            };
+
+            _orderDataWithCheckout = JsonConvert.SerializeObject(orderDataWithCheckout);
+            _orderDataWithMSI = JsonConvert.SerializeObject(orderDataWithMSI);
+            _orderDataWithSavedCard = JsonConvert.SerializeObject(orderDataWithSavedCard);
+        }
+
+        #endregion
+
+        #region :: Checkout components ::
+
+        /// <summary>
+        /// Successful order create.
+        /// </summary>
+        [Test()]
+        public void Successful_Order_Create_Test_OK()
+        {
+            Api.version = "2.0.0";
+            Api.apiKey = _apikey;
+
+            var order = new Order().create(_orderDataWithCheckout);
+
+            Assert.AreEqual(typeof(Checkout), order.checkout.GetType());
+            Assert.AreEqual(false, string.IsNullOrEmpty(order.checkout.id));
+            Assert.AreEqual("Integration", order.checkout.type);
+            Assert.AreEqual(false, order.checkout.monthly_installments_enabled);
+        }
+
+        /// <summary>
+        /// With checkout components monthly installments create.
+        /// </summary>
+        [Test()]
+        public void With_Checkout_Components_Monthly_Installments_Create_Test_OK()
+        {
+            Api.version = "2.0.0";
+            Api.apiKey = _apikey;
+
+            var order = new Order().create(_orderDataWithMSI);
+
+            Assert.AreEqual(typeof(Checkout), order.checkout.GetType());
+            Assert.AreEqual(false, string.IsNullOrEmpty(order.checkout.id));
+            Assert.AreEqual("Integration", order.checkout.type);
+            Assert.AreEqual(true, order.checkout.monthly_installments_enabled);
+        }
+
+        /// <summary>
+        /// With checkout components save card create.
+        /// </summary>
+        [Test()]
+        public void With_Checkout_Components_Save_Card_Create_Test_OK()
+        {
+            Api.version = "2.0.0";
+            Api.apiKey = _apikey;
+
+            var order = new Order().create(_orderDataWithSavedCard);
+
+            Assert.AreEqual(typeof(Checkout), order.checkout.GetType());
+            Assert.AreEqual(false, string.IsNullOrEmpty(order.checkout.id));
+            Assert.AreEqual("Integration", order.checkout.type);
+            Assert.AreEqual(true, order.checkout.on_demand_enabled);
+        }
+
+        #endregion
+
+        public void getApiKey()
+        {
+            Api.apiKey = "key_ZLy4aP2szht1HqzkCezDEA";
         }
         private int RandomNumber(int min, int max, int seed = 0)
         {
@@ -1322,5 +1495,354 @@ namespace ConektaTest
         }
 
 
+    }
+
+    /// <summary>
+    /// Checkout test.
+    /// </summary>
+    [TestFixture()]
+    public class CheckoutTest
+    {
+        #region :: Private Fields ::
+
+        /// <summary>
+        /// Api Key.
+        /// </summary>
+        private readonly string _apiKey = "key_ZLy4aP2szht1HqzkCezDEA";
+
+        /// <summary>
+        /// Checkout data.
+        /// </summary>
+        private readonly string _checkoutData;
+
+        /// <summary>
+        /// Checkout invalid expired at.
+        /// </summary>
+        private readonly string _checkoutInvalidExpiredAt;
+
+        /// <summary>
+        /// Checkout with MSI.
+        /// </summary>
+        private readonly string _checkoutDataWithMSI;
+
+        /// <summary>
+        /// Checkout with recurrent.
+        /// </summary>
+        private readonly string _checkoutDataRecurrent;
+
+        #endregion
+
+        #region :: Constructor ::
+
+        /// <summary>
+        /// Creates an instance of CheckoutTest.
+        /// </summary>
+        public CheckoutTest()
+        {
+            Api.version = "2.0.0";
+            Api.apiKey = _apiKey;
+
+            var customerInfo = new
+            {
+                name = "John Constantine",
+                phone = "4493424352",
+                email = "hola@hola.com"
+            };
+
+            var lineItems = new[]
+            {
+                new
+                {
+                    name = "Box of Cohiba S1s",
+                    unit_price = 35000,
+                    quantity = 1,
+                }
+            };
+
+            var orderTemplate = new
+            {
+                currency = "MXN",
+                line_items = lineItems,
+                customer_info = customerInfo,
+                metadata = new
+                {
+                    mycustomkey = "12345",
+                    othercustomkey = "abcde",
+                }
+            };
+
+            var orderTemplateNoCustomer = new
+            {
+                currency = "MXN",
+                line_items = lineItems,
+                metadata = new
+                {
+                    mycustomkey = "12345",
+                    othercustomkey = "abcde",
+                }
+            };
+
+            var checkoutData = new
+            {
+                name = "Payment Link Name",
+                type = "PaymentLink",
+                recurrent = false,
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                needs_shipping_contact = false,
+                order_template = orderTemplate
+            };
+
+            var checkoutInvalidExpiredAt = new
+            {
+                name = "Payment Link Name",
+                type = "PaymentLink",
+                recurrent = false,
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(3600)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                needs_shipping_contact = false,
+                order_template = orderTemplate
+            };
+
+            var checkoutDataWithMSI = new
+            {
+                name = "Payment Link Name",
+                type = "PaymentLink",
+                recurrent = false,
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                needs_shipping_contact = false,
+                monthly_installments_enabled = true,
+                monthly_installments_options = new int[] { 3, 6, 12 },
+                order_template = orderTemplate
+            };
+
+            var checkoutDataRecurrent = new
+            {
+                name = "Payment Link Name",
+                type = "PaymentLink",
+                recurrent = true,
+                payments_limit_count = 10,
+                expired_at = (int)(DateTime.UtcNow
+                    .AddSeconds(259200)
+                    .Subtract(new DateTime(1970, 1, 1)))
+                    .TotalSeconds,
+                allowed_payment_methods = new string[] { "cash", "card", "bank_transfer" },
+                needs_shipping_contact = false,
+                order_template = orderTemplateNoCustomer
+            };
+
+            _checkoutData = JsonConvert.SerializeObject(checkoutData);
+            _checkoutInvalidExpiredAt = JsonConvert.SerializeObject(checkoutInvalidExpiredAt);
+            _checkoutDataWithMSI = JsonConvert.SerializeObject(checkoutDataWithMSI);
+            _checkoutDataRecurrent = JsonConvert.SerializeObject(checkoutDataRecurrent);
+        }
+
+        #endregion
+
+        #region :: Methods ::
+
+        #region :: Creating payment link ::
+
+        /// <summary>
+        /// Creating payment link.
+        /// </summary>
+        [Test()]
+        public void Creating_Payment_Link_Test_OK()
+        {
+            var checkout = new Checkout().create(_checkoutData);
+
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.id));
+            Assert.AreEqual("checkout", checkout._object);
+            Assert.AreEqual(36, checkout.id.ToString().Length);
+            Assert.AreEqual("PaymentLink", checkout.type);
+            Assert.AreEqual(false, checkout.livemode);
+            Assert.AreEqual(false, checkout.needs_shipping_contact);
+            Assert.AreEqual(false, checkout.monthly_installments_enabled);
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.slug));
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.url));
+        }
+
+        /// <summary>
+        /// Unsuccessful checkout create.
+        /// </summary>
+        [Test()]
+        public void Unsuccessful_Checkout_Create_Test_OK()
+        {
+            Assert.Throws<ConektaException>(() =>
+            {
+                var checkout = new Checkout().create(string.Empty);
+            });
+        }
+
+        /// <summary>
+        /// Unsuccessful checkout expired at create.
+        /// </summary>
+        [Test()]
+        public void Unsuccessful_Checkout_Expired_At_Create_Test_OK()
+        {
+            Assert.Throws<ConektaException>(() =>
+            {
+                var checkout = new Checkout().create(_checkoutInvalidExpiredAt);
+            });
+        }
+
+        /// <summary>
+        /// Successful with monthly installments enabled.
+        /// </summary>
+        [Test()]
+        public void Successful_With_Monthly_Installments_Enabled_Test_OK()
+        {
+            var checkout = new Checkout().create(_checkoutDataWithMSI);
+
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.id));
+            Assert.AreEqual("checkout", checkout._object);
+            Assert.AreEqual(36, checkout.id.ToString().Length);
+            Assert.AreEqual("PaymentLink", checkout.type);
+            Assert.AreEqual(false, checkout.livemode);
+            Assert.AreEqual(false, checkout.needs_shipping_contact);
+            Assert.AreEqual(true, checkout.monthly_installments_enabled);
+            Assert.AreEqual(3, checkout.monthly_installments_options.Length);
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.slug));
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.url));
+
+        }
+
+        /// <summary>
+        /// Successful with recurrent.
+        /// </summary>
+        [Test()]
+        public void Successful_With_Recurrent_Test_OK()
+        {
+            var checkout = new Checkout().create(_checkoutDataRecurrent);
+
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.id));
+            Assert.AreEqual("checkout", checkout._object);
+            Assert.AreEqual(36, checkout.id.ToString().Length);
+            Assert.AreEqual("PaymentLink", checkout.type);
+            Assert.AreEqual(false, checkout.livemode);
+            Assert.AreEqual(false, checkout.needs_shipping_contact);
+            Assert.AreEqual(true, checkout.recurrent);
+            Assert.AreEqual(10, checkout.payments_limit_count);
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.slug));
+            Assert.AreEqual(false, string.IsNullOrEmpty(checkout.url));
+        }
+
+        #endregion
+
+        #region :: Get ::
+
+        /// <summary>
+        /// Successfully gets checkout.
+        /// </summary>
+        [Test()]
+        public void Successfully_Gets_Checkout_Test_OK()
+        {
+            var id = new Checkout().create(_checkoutData).id;
+
+            var checkout = new Checkout().find(id);
+
+            Assert.AreEqual("checkout", checkout._object);
+            Assert.AreEqual("PaymentLink", checkout.type);
+        }
+
+        /// <summary>
+        /// Successful checkout where.
+        /// </summary>
+        [Test()]
+        public void Successful_Checkout_Where_Test_OK()
+        {
+            Checkout[] checkouts = new Checkout().where(new JObject());
+
+            Assert.AreEqual(typeof(Checkout[]), checkouts.GetType());
+            Assert.AreEqual("checkout", checkouts.FirstOrDefault()._object);
+            Assert.AreEqual("PaymentLink", checkouts.FirstOrDefault().type);
+        }
+
+        #endregion
+
+        #region :: Notifications with payment link ::
+
+        /// <summary>
+        /// Unsuccessful send a checkout sms.
+        /// </summary>
+        [Test()]
+        public void Unsuccessful_Send_A_Checkout_Sms_Test_OK()
+        {
+            Assert.Throws<ConektaException>(() =>
+            {
+                var checkout = new Checkout().create(_checkoutData);
+
+                var parameters = new
+                {
+                    phone = "5555555555"
+                };
+
+                Assert.AreEqual(0, checkout.sms_sent);
+
+                checkout.send_sms(JsonConvert.SerializeObject(parameters));
+            });
+        }
+
+        /// <summary>
+        /// Successfully send a checkout email.
+        /// </summary>
+        [Test()]
+        public void Successfully_Send_A_Checkout_Email_Test_OK()
+        {
+            var checkout = new Checkout().create(_checkoutData);
+
+            var parameters = new
+            {
+                email = "test@mail.com"
+            };
+
+            Assert.AreEqual(0, checkout.emails_sent);
+
+            checkout.send_email(JsonConvert.SerializeObject(parameters));
+
+            var checkoutFound = new Checkout().find(checkout.id);
+
+            Assert.AreEqual(1, checkoutFound.emails_sent);
+        }
+
+        #endregion
+
+        #region :: Cancel checkout request ::
+
+        /// <summary>
+        /// Successful cancel a checkout.
+        /// </summary>
+        [Test()]
+        public void Successful_Cancel_A_Checkout_Test_OK()
+        {
+            var checkout = new Checkout().create(_checkoutData);
+
+            Assert.AreEqual("Issued", checkout.status);
+
+            var parameters = new
+            {
+                checkout.id
+            };
+
+            checkout.cancel(JsonConvert.SerializeObject(parameters));
+
+            var checkoutFound = new Checkout().find(checkout.id);
+
+            Assert.AreEqual("Cancelled", checkoutFound.status);
+        }
+
+        #endregion
+
+        #endregion
     }
 }
