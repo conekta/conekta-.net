ab6b4d933785c16be1dd169189e55b2c0bb5b77b
Nicolás Cedrón <josenicolascedron@gmail.com>
2025-04-29 10:47:56 -0300

    Build tests

--- src/Conekta.net.Test/Api/BaseTest.cs
+++
@@ -1,8 +0,0 @@
-namespace Conekta.net.Test.Api;
-
-using System;
-using System.IO;
-public class BaseTest
-{
-    public static readonly string BasePath = Environment.GetEnvironmentVariable("BASE_PATH") ?? "http://localhost:3000";
-}
